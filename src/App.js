9c45b5635adada33c1f3f3d1f389b00f48572e9b
muxammadazizjon <mazizmaxamatov@gmail.com>
2025-02-18 19:15:27 +0500

    salom

--- src/App.js
+++ src/App.js
@@ -1,53 +1,48 @@
-
+import { useEffect, useState } from 'react';
 import { Fragment } from 'react';
 import './App.css';
 import Header from './components/header';
 
 
-function App() {
-  
 
- 
+function App() {
+
+  
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(response => response.json())
+      .then(json => setData(json));
+  }, []);
+
+
+  console.log(data);
+
+
   return (
     <Fragment>
 
       <Header />
-      
+
+      <div className='App max-w-[1440px] mx-auto' >
+
+
+        {data.map((post) => {
+          return (
+            <div className='border bg-[black] text-[#FFFFFF]'>
+              <h1>{post.category}</h1>
+              <h2>{post.price}</h2>
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
+            </div>
+          )
+        })}
+      </div>
+
     </Fragment>
   );
 }
 
 
 export default App;
-
-
-
-
-
-
-
-
-
-/*
-const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch('https://fakestireapi.com/products')
-      .then(response => response.json())
-      .then(json => setData(json));
-  }, []);
-  console.log(data);
-
-
-<div className='App bg-homeBg'>
-      <h2>Hello</h2>
-
-      {data.map((post) => {
-        <div className='border bg-red-400'>
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-        </div>
-      })}
-
-    </div>
-    */
